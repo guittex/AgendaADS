4d400c591c1faecba99c5dc6282d87cd18c36f95
pablo ferreira <pabloferr92@gmail.com>
2018-03-19 11:00:59 -0300

    Simulando erro na funcao de testes

--- adicionar_test.py
+++ adicionar_test.py
@@ -4,4 +4,4 @@ def test_adicionar_sucesso ():
     assert funcoes.adicionar('pablo','12345678') == True
 
 def test_adicionar_falha ():
-    assert funcoes.adicionar('pablo', '1234') == False
+    assert funcoes.adicionar('pablo', '1234') == True
